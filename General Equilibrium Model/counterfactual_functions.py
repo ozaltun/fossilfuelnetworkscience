3e1a8fb69c7411bdbda4955aa3d151dc3ae35054
Bora Ozaltun <ozaltun@mit.edu>
2020-02-10 23:26:49 -0500

    Finsihing up the model draft

--- General Equilibrium Model/counterfactual_functions.py
+++ General Equilibrium Model/counterfactual_functions.py
@@ -1,55 +1,191 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from helper import *
 ## These functions have had a basic "check".
 def get_E_hat(w_hat, r_hat, data):
     ''' This is a function that returns the total expenditure change for all countries as a vector E_hat.
     variables: w_hat, r_hat
-    parameters: data['e_L'], data['e_R']
+    parameters: data['e_L'], data['e']
     output: E_hat (nx1)
     '''
-    E_hat = w_hat*data['e_L'] + (r_hat * data['e_R']) @ np.ones((data['g'], 1))
+    E_hat = w_hat*data['e_L'] + (r_hat * data['e']) @ np.ones((data['g'], 1))
     return E_hat
 
-
-def get_P_f_hat(P_f_goods_hat, data):
-    ''' This is a function that returns the price index change for a country as a vector P_f_hat.
-    variables: P_f_goods_hat
+def get_P_k_hat(P_k_goods_hat, data):
+    ''' This is a function that returns the price index change for a country as a vector P_k_hat.
+    variables: P_k_goods_hat
     parameters: data['alpha'], data['sigma']
-    output: P_f_hat (nx1)
+    output: P_k_hat (nx1)
     '''
-    new_matrix = data['alpha'] * (P_f_goods_hat)**(1-data['sigma'])
-    P_f_hat = (new_matrix @ np.ones((data['k'], 1)) )**(1/(1-data['sigma']))
+    new_matrix = data['alpha'] * (P_k_goods_hat)**(1-data['sigma'])
+    P_k_hat = (new_matrix @ np.ones((data['k'], 1)) )**(1/(1-data['sigma']))
 
-    return P_f_hat
+    return P_k_hat
 
-def get_P_f_goods_hat(C_f_hat, data):
+def get_P_k_goods_hat(C_k_hat, data):
     ''' This is a function that returns the price index for final goods change for a country and final good matrix.
-    variables: C_f_hat
-    parameters: data['lambda_f'], data['theta_k']
-    output: P_f_goods_hat (nxk)
+    variables: C_k_hat
+    parameters: data['lambda_k'], data['theta_k']
+    output: P_k_goods_hat (nxk)
     '''
 
-    P_f_goods_hat = np.zeros((data['n'], data['k']))
-    C_f_hat_temp = C_f_hat ** (-data['theta_k'].reshape((1, data['k'])))
-    part1 = np.sum(data['lambda_f'] * C_f_hat_temp.reshape((1, data['n'], data['k'])), axis=1)
+    P_k_goods_hat = np.zeros((data['n'], data['k']))
+    C_k_hat_temp = C_k_hat ** (-data['theta_k'].reshape((1, data['k'])))
+    part1 = np.sum(data['lambda_k'] * C_k_hat_temp.reshape((1, data['n'], data['k'])), axis=1)
     part2 = part1.reshape((data['n'], data['k']))
-    P_f_goods_hat = part2 ** (-1/data['theta_k'].reshape((1, data['k'])))
+    P_k_goods_hat = part2 ** (-1/data['theta_k'].reshape((1, data['k'])))
 
-    return P_f_goods_hat
+    return P_k_goods_hat
 
-def get_P_c_goods_hat(C_g_hat, data):
+def get_P_g_goods_hat(C_g_hat, data):
     ''' This is a function that returns the price index for change inn commodities for a country and commodity matrix.
     variables: C_g_hat
-    parameters: data['lambda_c'], data['theta_g']
+    parameters: data['lambda_g'], data['theta_g']
     output: P_g_goods_hat (nxg)
     '''
 
     P_g_goods_hat = np.zeros((data['n'], data['g']))
     C_g_hat_temp = C_g_hat ** (-data['theta_g'].reshape((1, data['g'])))
-    part1 = np.sum(data['lambda_c'] * C_g_hat_temp.reshape((1, data['n'], data['g'])), axis=1)
+    part1 = np.sum(data['lambda_g'] * C_g_hat_temp.reshape((1, data['n'], data['g'])), axis=1)
     part2 = part1.reshape((data['n'], data['g']))
     P_g_goods_hat = part2 ** (-1/data['theta_g'].reshape((1, data['g'])))
 
     return P_g_goods_hat
+
+def get_D_k_hat(E_hat, P_k_goods_hat, P_k_hat, data):
+    ''' This is a function that returns the demand for final goods for a country.
+    variables: E_hat, P_k_goods_hat, P_k_hat
+    parameters: data['sigma']
+    output: D_k_hat (nxk)'''
+
+    D_k_hat = E_hat * (P_k_goods_hat/P_k_hat) ** (1 - data['sigma'])
+
+    return D_k_hat
+
+def get_D_g_hat(P_g_goods_hat, C_k_hat, Y_k_hat, data):
+    ''' This is a function that returns the demand for commodities for a country.
+    variables: P_g_goods_hat, C_k_hat, Y_k_hat
+    parameters: data['d'], data['eta']
+    output: D_c_hat (nxg)
+
+    TODO: Problem with reshape
+
+    '''
+    C_k_hat_temp = C_k_hat **(data['eta'].reshape(1, data['k']) - 1)
+
+    part_1 = data['d'] * ( C_k_hat_temp * Y_k_hat ).reshape((data['n'], data['k'], 1))
+    part_2 = P_g_goods_hat.reshape((data['n'], 1, data['g'])) **(1 - data['eta'].reshape((1, data['k'], 1)))
+    D_g_hat = np.sum(part_1*part_2, axis=1).reshape((data['n'], data['g']))
+
+    return D_g_hat
+
+def get_Y_k_hat(C_k_hat, P_k_hat, D_k_hat, data):
+    ''' This is a function that returns the total production for a given final good and country.
+    variables: C_k_hat, P_k_hat, D_k_hat
+    parameters: data['X_k'], data['Y_k'], data['theta_k']
+    output: Y_k_hat (nxk)'''
+
+    C_k_hat_temp = C_k_hat **(-data['theta_k'].reshape((1, data['k'])))
+    P_k_hat_temp = P_k_hat **(data['theta_k'].reshape((1, data['k'])))
+
+    part_1 = data['X_k'] * (P_k_hat_temp * D_k_hat).reshape((data['n'],1,data['k']))
+
+    Y_k_hat = C_k_hat_temp/data['Y_k'] * np.sum(part_1, axis=1).reshape(data['n'], data['k'])
+
+    return Y_k_hat
+
+def get_Y_g_hat(C_g_hat, P_g_goods_hat, D_g_hat, data):
+    ''' This is a function that returns the total production for a given final good and country.
+    variables: C_g_hat, P_g_goods_hat, D_g_hat
+    parameters: data['X_g'], data['Y_g'], data['theta_g']
+    output: Y_g_hat (nxg)'''
+
+    C_g_hat_temp = C_g_hat **(-data['theta_g'].reshape((1, data['g'])))
+    P_g_goods_hat_temp = P_g_goods_hat **(data['theta_g'].reshape((1, data['g'])))
+
+    part_1 = data['X_g'] * (P_g_goods_hat_temp * D_g_hat).reshape((data['n'],1,data['g']))
+
+    Y_g_hat = C_g_hat_temp/data['Y_g'] * np.sum(part_1, axis=1).reshape(data['n'], data['g'])
+
+    return Y_g_hat
+
+def get_C_k_hat(w_hat, P_g_goods_hat, data):
+    ''' This is a function that returns the cost of final good in a country.
+    variables: w_hat, P_g_goods_hat
+    parameters: data['phi_L_k'], data['phi'], data['eta']
+    output: C_k_hat (nxk)'''
+
+    part1 = data['phi_L_k'] * (w_hat.reshape((data['n'], 1)) ** (1 - data['eta'].reshape((1, data['k']))))
+    part2 = data['phi'] * ((P_g_goods_hat.reshape(data['n'], 1, data['g'])) ** (1 - data['eta'].reshape((1, data['k'],1))) )
+
+    C_k_hat = (part1 + np.sum(part2, axis=2).reshape((data['n'], data['k']))) ** (1/(1-data['eta'].reshape((1, data['k'])) ))
+
+    return C_k_hat
+
+def get_C_g_hat(w_hat, r_hat, data):
+    ''' This is a function that returns the cost of Commodities in a country.
+    variables: w_hat, r_hat
+    parameters: data['phi_L_g'], data['phi_R'], data['rho_g']
+    output: C_G_hat (nxg)
+    '''
+
+    part1 = data['phi_R'] * (r_hat ** (1- data['rho_g'].reshape((1, data['g']))))
+    part2 = data['phi_L_g'] * (w_hat.reshape((data['n'], 1)) ** (1 - data['rho_g'].reshape((1, data['g']))))
+
+    C_g_hat = (part1 + part2) ** (1/(1-data['rho_g'].reshape((1, data['g'])) ))
+
+    return C_g_hat
+
+def get_r_hat(Y_g_hat, C_g_hat, data):
+    ''' This is a function that returns the ...
+    variables: Y_g_hat, C_g_hat
+    parameters: data['R_hat'], data['rho_g']
+    output: r_hat (nxg)'''
+
+    part1 = (Y_g_hat/data['R_hat']) ** (1/data['rho_g'].reshape((1, data['g'])))
+    part2 = (C_g_hat) ** (1 - 1/data['rho_g'].reshape((1, data['g'])))
+
+    r_hat = part1*part2
+
+    return r_hat
+
+
+def get_w_hat(w_hat, Y_g_hat, Y_k_hat, C_k_hat, C_g_hat, data):
+    part1_part1 = data['phi_L_g']*data['Y_g']/(data['E']*data['e_L']).reshape((data['n'], 1)) * ((w_hat.reshape((data['n'], 1))/ \
+                    C_g_hat)**(1-data['rho_g'].reshape((1, data['g'])))) *Y_g_hat
+    part1 = part1_part1.sum(axis=1)
+
+    part2_part1 = data['phi_L_k']*data['Y_k']/(data['E']*data['e_L']).reshape((data['n'], 1)) * ((w_hat.reshape((data['n'], 1))/ \
+                    C_k_hat)**(1-data['eta'].reshape((1, data['k'])))) *Y_k_hat
+
+    part2 = part2_part1.sum(axis=1)
+
+    w_hat_new = (part1 + part2).reshape((data['n'], 1))
+    return w_hat_new
+
+
+def counterfactual(X, data):
+    n = data['n']
+    g = data['g']
+    k = data['k']
+    E_hat, P_k_hat, P_k_goods_hat, P_g_goods_hat, D_k_hat, D_g_hat, Y_k_hat, Y_g_hat, C_k_hat, C_g_hat, r_hat, w_hat  = get_values_from_X(X, data)
+
+    values = []
+
+    values.append(E_hat - get_E_hat(w_hat, r_hat, data))
+    values.append(P_k_hat - get_P_k_hat(P_k_goods_hat, data))
+    values.append(P_k_goods_hat - get_P_k_goods_hat(C_k_hat, data))
+    values.append(P_g_goods_hat - get_P_g_goods_hat(C_g_hat, data))
+    values.append(D_k_hat - get_D_k_hat(E_hat, P_k_goods_hat, P_k_hat, data))
+    values.append(D_g_hat - get_D_g_hat(P_g_goods_hat, C_k_hat, Y_k_hat, data))
+    values.append(Y_k_hat - get_Y_k_hat(C_k_hat, P_k_hat, D_k_hat, data))
+    values.append(Y_g_hat - get_Y_g_hat(C_g_hat, P_g_goods_hat, D_g_hat, data))
+    values.append(C_k_hat - get_C_k_hat(w_hat, P_g_goods_hat, data))
+    values.append(C_g_hat - get_C_g_hat(w_hat, r_hat, data))
+    values.append(r_hat - get_r_hat(Y_g_hat, C_g_hat, data))
+    values.append(w_hat - get_w_hat(w_hat, Y_g_hat, Y_k_hat, C_k_hat, C_g_hat, data))
+
+    X_new = get_X_from_values(values, data)
+
+    return X_new
---
+++ General Equilibrium Model/helper.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+def save_obj(obj, loc ):
+    with open(loc, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(loc):
+    with open(loc, 'rb') as f:
+        return pickle.load(f)
+
+def reverse_dict(my_map):
+    inv_map = {v: k for k, v in my_map.items()}
+    return inv_map
+
+def convertRow2Matrix(df, index_cols, maps):
+    ''' This is a new function: example use:
+
+    vom_np_2 = convertRow2Matrix(vom, ['TRAD_COMM','REG'], [comm_2_num, reg_2_num])
+
+    '''
+    df_copy = df.copy()
+    df_copy = df_copy.groupby(index_cols).sum().reset_index()
+
+    index_cols_n = [col+'_Number' for col in index_cols]
+    for i, col in enumerate(index_cols):
+        df_copy[index_cols_n[i]] = df_copy[col].map(maps[i])
+
+    df_mi = df_copy.set_index(index_cols_n)
+    df_mi.drop(index_cols, axis=1, inplace=True)
+
+    shape = list(map(len, df_mi.index.levels))
+    output = np.full(shape, -1000.0)
+
+    df_mi.sort_index(inplace=True)
+
+    for i in list(df_mi.index.values):
+        output[i] = df_mi.loc[i].values[0]
+
+    print('Number of empty values: ', output[output<0])
+
+    return output
+
+def sort_gtap_data(file):
+    # Importing the data
+    data_dict = {}
+    with open(input_root.format(file)) as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            if 'Header' in row[0]:
+                key = row[2].split('description: ')[1]
+                data_dict[key] = {}
+                data_dict[key]['header']= row
+                data_dict[key]['data'] = []
+                print(row)
+            else:
+                data_dict[key]['data'].append(row)
+
+    # Reformatting the header
+    acronym_list = []
+    for i in data_dict.keys():
+        print(i)
+        data_dict[i]['header'][0] = data_dict[i]['header'][0].split('!Header: ')[1]
+
+    # Creating the output folder
+    output_folder = output_root.format(file[:-4].lower())
+    if not os.path.exists(output_folder):
+        print('New Directory Created: '+file[:-4].lower())
+        os.makedirs(output_folder)
+
+    # Turning the data into a DataFrame
+    for i in data_dict.keys():
+        data_dict[i]['data_df'] = pd.DataFrame(data_dict[i]['data'])
+
+    # Saving the data to csv files by the header name
+    header_list = []
+    for i in data_dict.keys():
+        output_file = output_folder + '/'+ data_dict[i]['header'][0] +'.csv'
+        data_dict[i]['data_df'].to_csv(output_file)
+        header_list.append(data_dict[i]['header'])
+
+    # Saving the data description
+    pd.DataFrame(header_list).to_excel(output_root.format(file[:-4]+'_data-description.xlsx'))
+
+def get_values_from_X(X, data):
+    n = data['n']
+    g = data['g']
+    k = data['k']
+
+    start = 0
+    E_hat = X[start:n].reshape((n, 1))
+    start += n
+    P_k_hat = X[start:(start + n)].reshape((n, 1))
+    start += n
+    P_k_goods_hat = X[start:(start+n*k)].reshape((n, k))
+    start += n*k
+    P_g_goods_hat = X[start:(start+n*g)].reshape((n, g))
+    start += +n*g
+    D_k_hat = X[start:(start+n*k)].reshape((n, k))
+    start += n*k
+    D_g_hat = X[start:(start+n*g)].reshape((n, g))
+    start += n*g
+    Y_k_hat = X[start:(start+n*k)].reshape((n, k))
+    start += n*k
+    Y_g_hat = X[start:(start+n*g)].reshape((n, g))
+    start += n*g
+    C_k_hat = X[start:(start+n*k)].reshape((n, k))
+    start += n*k
+    C_g_hat = X[start:(start+n*g)].reshape((n, g))
+    start += n*g
+    r_hat =  X[start:(start+n*g)].reshape((n, g))
+    start += n*g
+    w_hat = X[start:(start+n)].reshape((n, 1))
+    end = start + n
+
+    return E_hat, P_k_hat, P_k_goods_hat, P_g_goods_hat, D_k_hat, D_g_hat, Y_k_hat, Y_g_hat, C_k_hat, C_g_hat, r_hat, w_hat
+
+def get_X_from_values(values, data):
+    n = data['n']
+    g = data['g']
+    k = data['k']
+
+    # E_hat, P_k_hat, P_k_goods_hat, P_g_goods_hat, D_k_hat, D_g_hat, Y_k_hat, Y_g_hat, C_k_hat, C_g_hat, r_hat, w_hat = values
+
+    X = np.zeros(5*n*g + 4*n*k + 3*n)
+
+    start = 0
+    end = 0
+    for i in values:
+        temp = np.ravel(i)
+        end += temp.shape[0]
+        X[start:end] = temp
+        start = end
+
+    return X
